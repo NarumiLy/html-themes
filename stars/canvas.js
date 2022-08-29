acf604f6f7ca75acf1ea287c5a46fa4d9a558844
Lyciaa <compterpdayle@gmail.com>
2022-08-29 14:22:58 +0200

    Background gradient

--- stars/canvas.js
+++ stars/canvas.js
@@ -15,15 +15,15 @@ window.onload = function () {
         canvas.setAttribute('width', style_width * dpi);
         // thanks to Zak Frisch
     }
-    function stars(x, y, r) {
+    function stars(x, y, r, count) {
         this.y = y;
         this.x = x;
         this.r = r;
+        this.count = count;
         ctx.fillStyle = "white";
         let circle = Math.PI * 2;
         ctx.beginPath();
         ctx.arc(x, y, r, 0, circle, false);
-        ctx.stroke()
         ctx.fill()
     }
 
@@ -31,14 +31,20 @@ window.onload = function () {
 
         this.y -= 0.20;
 
+        /* if() {
+            this.x = this.x - 0.20;
+        } else {
+            this.x = this.x + 0.20;
+        } */
+        this.count += 1;
         if(this.y <= 0 ) this.y = canvas.height;
-        stars(this.x, this.y, this.r)
+        stars(this.x, this.y, this.r, this.count)
 
     }
     let i = 0;
     let star = [];
     while (i < 50) {
-        star[i] = new stars(Math.abs(Math.floor(Math.random() * 1900)), Math.abs(Math.floor(Math.random() * 1050)), Math.floor(Math.random() * 3)+1);
+        star[i] = new stars(Math.abs(Math.floor(Math.random() * 1900)), Math.abs(Math.floor(Math.random() * 1050)), Math.floor(Math.random() * 3)+1, 0);
         i++;
     }
 
